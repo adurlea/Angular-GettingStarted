77defe2952d764f3f6f7a968b2ee7e778f9bf923
DURLEA Adrian <adrian.durlea@gmail.com>
2019-12-02 14:17:53 +0100

    Chapters 11-13 Implementation: - Building nested components, passing data in and out an nested component - Creating services, registering, injection - Retreivind data using HTTP, subscribe observable, asynch, handeling exceptions - Navigation and routing, how, config, place view, pasing parameters, activate route with code, guards

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,46 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent implements OnInit{
-  pageTitle: string = 'Page List';
+  pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   _listFilter: string;
   filteredProducts: IProduct [];
+  products: IProduct[];
+  errorMessage: string;
 
-  products: IProduct[] = [
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2019",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      'imageUrl': "assets/images/saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2018",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
-  ];
-
-  constructor () {
-    this.filteredProducts = this.products;
-    this._listFilter = 'game';
+  constructor(private productService: ProductService) {
   }
 
   get listFilter() {
@@ -57,12 +34,22 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List: " + message;
   }
 }
